f1d9588d05b6e82676868d2f464fee01df454187
Mary Castorani <mgcastorani@gmail.com>
2024-05-06 14:51:25 -0600

    problem 7 and 8 complete and going slightly insane

--- practice.js
+++ practice.js
@@ -186,7 +186,7 @@ getUserById(users, '16t', (user) => {
 // Make sure to use an arrow function.
 // You should not use a for loop, but should use the filter method instead.
 
-// REPLACE THIS WITH YOUR CODE
+let evens = (array) => array.filter((num) => num % 2 === 0);
 
 /// /////// PROBLEM 8 //////////
 
@@ -198,7 +198,7 @@ getUserById(users, '16t', (user) => {
 //
 // Make sure to use an arrow function combined with the filter method (not a for loop).
 
-// REPLACE THIS WITH YOUR CODE
+let startWithLetterA = (array) => array.filter((str) => str[0].toLowerCase() === 'a')
 
 /// /////// PROBLEM 9 //////////
 
