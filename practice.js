dd3b0a99f174cd66c59de3dc144a3cadf6d7d0e4
Mary Castorani <mgcastorani@gmail.com>
2024-05-06 15:13:05 -0600

    finished, mama mia..

--- practice.js
+++ practice.js
@@ -239,7 +239,11 @@ const employees = [
 ];
 // Do not edit the code above.
 
-// REPLACE THIS WITH YOUR CODE
+const findProgrammer = (emps) => {
+  const programmers = emps.filter((emp) => emp.job === 'programmer');
+  return programmers[0];
+};
+
 
 ////////// PROBLEM 12 //////////
 
@@ -266,7 +270,7 @@ const orders = [
   Example: if tax is 0.07, the price afterTax could be calculated like this: afterTax = price * 1.07)
 */
 
-let orderTotals; // Code here
+let orderTotals = orders.map((order) => order.price * (1 + order.tax))
 
 /// /////// PROBLEM 13 //////////
 
@@ -282,7 +286,9 @@ const exampleMenuItems = [
 // Create a function called sortMenuItems which takes in an array of objects like the one above
 // and sorts the array by price from smallest to largest. It should return the sorted array.
 
-// REPLACE THIS WITH YOUR CODE
+let sortMenuItems = (items) => {
+  return items.sort((item1, item2) => item1.price - item2.price);
+}
 
 /// /////// PROBLEM 14 //////////
 
@@ -292,4 +298,6 @@ const exampleMenuItems = [
 //
 // Make sure to use arrow functions combined with the reduce method.
 
-// REPLACE THIS WITH YOUR CODE
+let productOfArray = (numbers) => {
+  return numbers.reduce((notcurrent, current) => notcurrent *current);
+}
